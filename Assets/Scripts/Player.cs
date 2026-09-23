b61048a3cb5d1f0f569f29afde441668db967a4e
dgaferip <davigfrpinto@gmail.com>
2026-09-23 17:02:03 -0300

    Feat: Climb

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,72 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     CharacterController cc;
-    public float speed = 15f;
-    public float rotateSpeed = 1f;
-    public float jumpHeight = 1.5f;
     public float gravityValue = -9.81f;
-    public float fall;
+
     public bool isMoving; //Pública apenas para fins de testes
 
+    [Header("WALK")]
+    public float rotateSpeed = 130f;
+    public float walkSpeed = 10f;
+    public float jumpHeight = 2.5f;
+    public float fall;
+
+    [Header("CLIMB")]
+    public float climbSpeedV = 5f;
+    public float climbSpeedH = 5f;
+    public float obstacleMaxDistance = 0.6f;
+    public float climbLock = 0.3f;
+    private LayerMask obstacleLayer;
+    [SerializeField] private bool isClimbing;
+    private float climbLockTimer;
 
     private float idleTime = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameController.gameController.player = this;
         cc = GetComponent<CharacterController>();
+        obstacleLayer = LayerMask.GetMask("Obstacle");
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!GameController.gameController.isPlaying || GameController.gameController.isPaused) return;
-        Debug.Log("a");
-        Movement();
+
+        bool obstacleAhead = Physics.Raycast(transform.position, transform.forward, out RaycastHit obstacleHit, obstacleMaxDistance, obstacleLayer);
+
+        if (climbLockTimer > 0f)
+        {
+            climbLockTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && (cc.isGrounded || isClimbing))
+        {
+            fall = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+            isClimbing = false;
+            climbLockTimer = climbLock;
+        }
+        else if (obstacleAhead && !isClimbing && climbLockTimer <= 0f)
+        {
+            isClimbing = true;
+        }
+        else if (isClimbing && !obstacleAhead)
+        {
+            isClimbing = false;
+            climbLockTimer = climbLock;
+        }
+
+        if (isClimbing)
+        {
+            Climb();
+        }
+        else
+        {
+            Movement();
+        }
+
         DetectMovement();
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             GameController.gameController.uiController.PauseGame();
@@ -36,7 +78,7 @@ public class Player : MonoBehaviour
 
     private void DetectMovement()
     {
-        isMoving = cc.velocity.sqrMagnitude > 0.01f; 
+        isMoving = cc.velocity.sqrMagnitude > 0.01f;
 
         if (cc.velocity.magnitude <= 0)
         {
@@ -57,16 +99,9 @@ public class Player : MonoBehaviour
 
     private void Movement()
     {
-        Debug.Log("b");
-
         if (cc.isGrounded && fall < 0f)
         {
             fall = -2f;
-        }
-
-        if (cc.isGrounded && Input.GetButtonDown("Jump"))
-        {
-            fall = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
         }
 
         fall += gravityValue * Time.deltaTime;
@@ -74,19 +109,26 @@ public class Player : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         float r = Input.GetAxis("Horizontal");
 
-        if(v != 0 || r != 0) isMoving = true;
-        else isMoving = false;
+        isMoving = v != 0 || r != 0;
 
-        Vector3 dir = transform.forward * v * speed;
+        Vector3 dir = transform.forward * v * walkSpeed;
         dir.y = fall;
 
-        transform.Rotate(0f, r * rotateSpeed, 0f);
-        cc.Move(dir * Time.deltaTime);
+        transform.Rotate(0f, r * rotateSpeed * Time.deltaTime, 0f);
 
-        
+        cc.Move(dir * Time.deltaTime);
     }
 
-    
+    private void Climb()
+    {
+        fall = 0f;
 
+        float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Horizontal");
 
+        isMoving = v != 0 || r != 0;
+
+        Vector3 dir = transform.up * v * climbSpeedV + transform.right * r * climbSpeedH;
+        cc.Move(dir * Time.deltaTime);
+    }
 }
